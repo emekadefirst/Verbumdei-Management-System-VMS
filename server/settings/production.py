ab88e718eb3501b02fcfa29a01f34870b2a55321
emekadefirst <victorchibuogwu33@gmail.com>
2024-08-26 00:43:43 +0100

    vms

--- server/settings/production.py
+++ server/settings/production.py
@@ -32,7 +32,10 @@ DATABASES = {
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:3000"
+]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.example\.com$",
 ]
