ef96ada09ed1cf1fc9a8135e49e3e6377fc97823
emekadefirst <victorchibuogwu33@gmail.com>
2024-08-31 21:06:17 +0100

    incoming

--- server/settings/local.py
+++ server/settings/local.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "verbumdeiMS.db",
+        "NAME": BASE_DIR / "verbumdeiMS.db"
     }
 }
 
