ad16689ce777bb96b082dcdb94ca0f26698daa9e
emekadefirst <victorchibuogwu33@gmail.com>
2024-09-05 16:37:25 +0100

    hhh

--- student/serializers.py
+++ student/serializers.py
@@ -20,13 +20,12 @@ class StudentSerializer(serializers.ModelSerializer):
             "state_of_origin",
             "local_government_area",
             "nationality",
-            "registration_id",
             "parent",
             "religion",
             "profile_image",
             "class_assigned",
         ]
-        read_only_fields = ['registration_id', 'created_at']
+        read_only_fields = ('registration_id', 'created_at')
 
     def create(self, validated_data):
         student = Student.objects.create(**validated_data)
