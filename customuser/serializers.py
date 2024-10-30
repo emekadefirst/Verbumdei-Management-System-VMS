afb993448645412fac50d98ec54c979754dd64cb
emekadefirst <victorchibuogwu33@gmail.com>
2024-10-30 03:01:43 +0100

    admin

--- customuser/serializers.py
+++ customuser/serializers.py
@@ -11,38 +11,11 @@ def userId(firstname):
     now = datetime.now()
     return f"{firstname}{now.strftime('%M%S')}"
 
-    # def check_password(user_password):
-    #     password_pattern = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-    #     return bool(re.match(password_pattern, user_password))
-
-    # def validate_password(password):
-    #     if len(password) < 8:
-    #         raise ValidationError("Password must be at least 8 characters long.")
-    #     if not check_password(password):
-    #         raise ValidationError(
-    #             "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
-    #         )
-
-    # extra_kwargs = {
-    #     "password": {"write_only": True},
-    #     "created_at": {"read_only": True},
-    #     "role": {"read_only": True},
-    #     "email": {"read_only": True},
-    #     "first_name": {"read_only": True},
-    #     "last_name": {"read_only": True},
-    #     "username": {"read_only": True},
-    # }
-
-
-def userId(firstname):
-    now = datetime.now()
-    return f"{firstname}{now.strftime('%M%S')}"
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = [
+            "id", 
             "person_id",
             "username",
             "password",
@@ -53,6 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
         ]
         extra_kwargs = {
+            "id": {"read_only": True},
             "password": {"write_only": True},
             "created_at": {"read_only": True},
             "first_name": {"read_only": True},
@@ -70,13 +44,12 @@ class UserSerializer(serializers.ModelSerializer):
         try:
             staff = Staff.objects.get(staff_id=staff_id)
             if staff:
-                    first_name = staff.first_name
-                    last_name = staff.last_name
-                    email = staff.email
-                    username = userId(first_name.lower())
+                first_name = staff.first_name
+                last_name = staff.last_name
+                email = staff.email
+                username = userId(first_name.lower())
         except Staff.DoesNotExist:
             raise serializers.ValidationError({"error": "Staff ID does not exist"})
-        
 
         validated_data["first_name"] = first_name
         validated_data["last_name"] = last_name
